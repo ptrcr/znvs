50e88db3452078096fd74a6da6001df3e8c01643
ptrcr <16079472+ptrcr@users.noreply.github.com>
2024-03-06 08:53:23 +0100

    test: Add test sample

--- scripts/run_reference_app.sh
+++ scripts/run_reference_app.sh
@@ -5,4 +5,4 @@ export ZEPHYR_BASE=$(pwd)/test/zephyr
 source test/zephyr/zephyr-env.sh
 cd $BUILD_DIR
 # Run app and dump NVS region memory to terminal
-echo " flash read 0x1000 0xB00" | ninja run > dump.txt && sleep 2
+( echo -n; sleep 2; echo "flash read 0x1000 0xB00") | ninja run > dump.txt
