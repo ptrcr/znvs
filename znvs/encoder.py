3678bb3d734b70c186f80c3525af28e6ecd19531
ptrcr <16079472+ptrcr@users.noreply.github.com>
2024-04-02 08:27:00 +0200

    feat: Add encoder of whole NVS

--- znvs/encoder.py
+++ znvs/encoder.py
@@ -1,7 +1,8 @@
-import logging
-from .entry import Entry
-from .exception import ParameterError
 from .nvs import Nvs
+from .exception import EncodingError
+from .entry import Entry
+import logging
+from .ate import Ate
 
 logger = logging.getLogger(__name__)
 
@@ -18,12 +19,30 @@ class Encoder:
         self.sector_size = sector_size
 
     def dump(self, entries: list[Entry]) -> bytes:
-        entries: dict[int, bytes] = {}
+        sectors = []
+        while len(entries) > 0:
+            sector = bytearray.fromhex("FF") * self.sector_size
+            ate = None
+            consumed: list[Entry] = []
+            for entry in entries:
+                if ate is None:
+                    ate = Ate(self.sector_size - Ate._SIZE, entry.id, entry.value, 0)
+                else:
+                    ate = ate.next(entry.id, entry.value)
 
+                try:
+                    ate.to_bytes(sector)
+                    consumed.append(entry)
+                except EncodingError as _:
+                    # No space in sector
+                    # TODO: Close sector
+                    # TODO: Validate it is possible to encode such entry in empty sector
+                    break
 
-        for sector in Nvs(data, self.sector_size):
-            for ate in sector:
-                entry = ate.get_entry()
-                entries[entry.id] = entry.value
+            # Remove entries that were successfully encoded
+            for entry in consumed:
+                entries.remove(entry)
 
-        return [Entry(id, data) for id, data in entries.items() if data is not None]
+            sectors.append(sector)
+
+        return b''.join(sectors)
